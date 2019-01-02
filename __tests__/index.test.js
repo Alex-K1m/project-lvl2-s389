86b823f3579ef6bc071e7c4d4f1832117851ee57
Alex Kim <alex.k6@mail.ru>
2019-01-02 21:55:46 +0500

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,16 +3,16 @@ import gendiff from '../src';
 
 describe('gendiff', () => {
   it('diff output 1', () => {
-    const pathToConfig1 = '../__tests__/__fixtures__/before.json';
-    const pathToConfig2 = '../__tests__/__fixtures__/after.json';
+    const pathToConfig1 = '__tests__/__fixtures__/before.json';
+    const pathToConfig2 = '__tests__/__fixtures__/after.json';
     const expected = fs.readFileSync('__tests__/__fixtures__/expected.txt')
       .toString();
     expect(gendiff(pathToConfig1, pathToConfig2)).toBe(expected);
   });
 
   it('diff output 2', () => {
-    const pathToConfig1 = '../__tests__/__fixtures__/before2.json';
-    const pathToConfig2 = '../__tests__/__fixtures__/after2.json';
+    const pathToConfig1 = '__tests__/__fixtures__/before2.json';
+    const pathToConfig2 = '__tests__/__fixtures__/after2.json';
     const expected = fs.readFileSync('__tests__/__fixtures__/expected2.txt')
       .toString();
     expect(gendiff(pathToConfig1, pathToConfig2)).toBe(expected);
