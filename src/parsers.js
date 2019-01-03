9e68f5c5285b3575d3844ce6d982250267e28462
Alex Kim <alex.k6@mail.ru>
2019-01-04 00:16:42 +0500

    refactor parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,17 +1,15 @@
 import yaml from 'js-yaml';
 
-const parsers = new Map();
-
-const defineParser = (extension, fn) => parsers.set(extension, fn);
-
-const getParser = extension => parsers.get(extension);
-
-const parse = (extension, fileContent) => getParser(extension)(fileContent);
-export default parse;
-
 const parseJson = fileContent => JSON.parse(fileContent);
-defineParser('.json', parseJson);
 
 const parseYaml = fileContent => yaml.safeLoad(fileContent);
-defineParser('.yaml', parseYaml);
-defineParser('.yml', parseYaml);
+
+const parsers = {
+  '.json': parseJson,
+  '.yaml': parseYaml,
+  '.yml': parseYaml,
+};
+
+const parse = (extension, file) => parsers[extension](file);
+
+export default parse;
