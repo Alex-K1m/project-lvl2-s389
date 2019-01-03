cf4a816dd8884a6e0d07524e44f7e986f9f749a3
Alex Kim <alex.k6@mail.ru>
2019-01-03 19:07:13 +0500

    refactor tests to support json & yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,18 +2,18 @@ import fs from 'fs';
 import gendiff from '../src';
 
 describe('gendiff', () => {
-  it('diff output 1', () => {
+  it('diff output json', () => {
     const pathToConfig1 = '__tests__/__fixtures__/before.json';
     const pathToConfig2 = '__tests__/__fixtures__/after.json';
-    const expected = fs.readFileSync('__tests__/__fixtures__/expected.txt')
+    const expected = fs.readFileSync('__tests__/__fixtures__/expectedJson.txt')
       .toString();
     expect(gendiff(pathToConfig1, pathToConfig2)).toBe(expected);
   });
 
-  it('diff output 2', () => {
-    const pathToConfig1 = '__tests__/__fixtures__/before2.json';
-    const pathToConfig2 = '__tests__/__fixtures__/after2.json';
-    const expected = fs.readFileSync('__tests__/__fixtures__/expected2.txt')
+  it('diff output yaml', () => {
+    const pathToConfig1 = '__tests__/__fixtures__/before.yaml';
+    const pathToConfig2 = '__tests__/__fixtures__/after.yml';
+    const expected = fs.readFileSync('__tests__/__fixtures__/expectedYaml.txt')
       .toString();
     expect(gendiff(pathToConfig1, pathToConfig2)).toBe(expected);
   });
