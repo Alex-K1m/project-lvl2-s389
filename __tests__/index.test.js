658aa2014d9ff51928824688fda1eb84f6e157a3
Alex Kim <alex.k6@mail.ru>
2019-01-03 20:29:33 +0500

    add ini test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,19 +1,30 @@
 import fs from 'fs';
+import path from 'path';
 import gendiff from '../src';
+
+const fixtures = '__tests__/__fixtures__/';
 
 describe('gendiff', () => {
   it('diff output json', () => {
-    const pathToConfig1 = '__tests__/__fixtures__/before.json';
-    const pathToConfig2 = '__tests__/__fixtures__/after.json';
-    const expected = fs.readFileSync('__tests__/__fixtures__/expectedJson.txt')
+    const pathToConfig1 = path.join(fixtures, 'before.json');
+    const pathToConfig2 = path.join(fixtures, 'after.json');
+    const expected = fs.readFileSync(path.join(fixtures, 'expectedJson.txt'))
       .toString();
     expect(gendiff(pathToConfig1, pathToConfig2)).toBe(expected);
   });
 
   it('diff output yaml', () => {
-    const pathToConfig1 = '__tests__/__fixtures__/before.yaml';
-    const pathToConfig2 = '__tests__/__fixtures__/after.yml';
-    const expected = fs.readFileSync('__tests__/__fixtures__/expectedYaml.txt')
+    const pathToConfig1 = path.join(fixtures, 'before.yaml');
+    const pathToConfig2 = path.join(fixtures, 'after.yml');
+    const expected = fs.readFileSync(path.join(fixtures, 'expectedYaml.txt'))
+      .toString();
+    expect(gendiff(pathToConfig1, pathToConfig2)).toBe(expected);
+  });
+
+  it('diff output ini', () => {
+    const pathToConfig1 = path.join(fixtures, 'before.ini');
+    const pathToConfig2 = path.join(fixtures, 'after.ini');
+    const expected = fs.readFileSync(path.join(fixtures, 'expectedIni.txt'))
       .toString();
     expect(gendiff(pathToConfig1, pathToConfig2)).toBe(expected);
   });
