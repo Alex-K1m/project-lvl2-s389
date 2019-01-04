b5d778d2838243b18b46fa6ef2dbe2806cc45e3f
Alex Kim <alex.k6@mail.ru>
2019-01-05 00:20:20 +0500

    add ast builder

--- src/index.js
+++ src/index.js
@@ -9,34 +9,47 @@ const getData = (pathToFile) => {
   return parse(extension, file);
 };
 
-const buildLine = (str, key, value, mark = ' ') => `${str}\n  ${mark} ${key}: ${value}`;
+// AST consists of these objects:
+// { type: element, status: same | added | removed, key, value }
+// { type: list, key, children }
 
-// {
-//   status: not changed | added | changed | removed,
-//   key: ___,
-//   value: ___ | []
-// }
+const makeElement = (status, key, value) => ({
+  type: 'element',
+  status,
+  key,
+  value,
+});
+
+const genAst = (obj1, obj2) => {
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
+
+  return keys.reduce((acc, key) => {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return [...acc, { type: 'list', key, children: genAst(value1, value2) }];
+    }
+
+    if (_.has(obj1, key) && _.has(obj2, key)) {
+      if (value1 === value2) {
+        return [...acc, makeElement('same', key, value1)];
+      }
+      return [...acc,
+        makeElement('removed', key, value1),
+        makeElement('added', key, value2)];
+    }
+    if (_.has(obj1, key)) {
+      return [...acc, makeElement('removed', key, value1)];
+    }
+    return [...acc, makeElement('added', key, value2)];
+  }, []);
+};
 
 const gendiff = (path1, path2) => {
-  const file1 = getData(path1);
-  const file2 = getData(path2);
-  const keys = _.union(_.keys(file1), _.keys(file2));
-
-  const str = keys.reduce((acc, key) => {
-    if (file1[key] === file2[key]) {
-      return buildLine(acc, key, file1[key]);
-    }
-    if (_.has(file1, key) && _.has(file2, key)) {
-      const newAcc = buildLine(acc, key, file1[key], '-');
-      return buildLine(newAcc, key, file2[key], '+');
-    }
-    if (_.has(file1, key)) {
-      return buildLine(acc, key, file1[key], '-');
-    }
-    return buildLine(acc, key, file2[key], '+');
-  }, '');
-
-  return `{${str}\n}`;
+  const data1 = getData(path1);
+  const data2 = getData(path2);
+  console.log(genAst(data1, data2));
 };
 
 export default gendiff;
